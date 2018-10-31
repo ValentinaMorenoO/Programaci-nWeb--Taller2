8779061d0848d677d873c8e2d57336aee2318e04
ValentinaMO <valentina02051998@gmail.com>
2018-10-30 19:01:59 -0500

    Productos agregados a la base de datos

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const express= require('express');
 const engines= require('consolidate');
 const handlebars= require ('handlebars');
 const MongoClient= require('mongodb').MongoClient;
+const bodyParser= require('body-parser');
+const hbs = require('express-handlebars');
+
+
+const dbName= 'ropa';
+
 
 var app = express();
-var db;
+var db= null;
 
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
@@ -15,6 +21,40 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 var port = 3000;
+
+app.get('/producto', (req, res) => {
+    res.render('producto');
+});
+
+app.listen(port);
+
+const assert = require("assert");
+const url = "mongodb://localhost:27017";
+const mongoName = "productos";
+const mongoClient = new MongoClient(url);
+
+var products = null;
+
+mongoClient.connect(function (err) {
+
+    assert.equal(null, err);
+    console.log("equis de");
+
+    const db = mongoClient.db(mongoName);
+    products = db.collection("Ropa");
+});
+
+mongoClient.connect(function(err){
+
+    if(err){
+        console.error(err);
+        return;
+    }
+    db= mongoClient.db(dbName);
+
+});
+
+
 
 /*MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err) throw err;
@@ -27,19 +67,13 @@ var port = 3000;
 
 */
 
-app.get('/', (req, res) => {
+app.get("/", function(req, res){
+    products.find({}).toArray(function(err, array){
+        var context ={
+            producto: array,
+        };
 
-    //var dbColectionName = db.collection('dbColectionName')
-     //   .find();
-
-    //dbColectionName.toArray((err, result) => {
-
-        res.render('index', {
-            //array: result,
-
-        //});
+        console.log(array);
+        res.render("index", context);
     });
-
 });
-
-app.listen(port );
---
+++ productos.js
@@ -0,0 +1,140 @@
+.insertMany([
+    {
+        imagen: "/imagenes/ropa/blusasM/1.jpg",
+        nombre: "Join Life",
+        precio: 20.000,
+        categoria: "Mujer",
+        tipo: "Ropa",
+        valor: "bajo",
+    },
+    {
+        imagen: "/imagenes/ropa/blusasM/2.jpg",
+        nombre: "Join Life",
+        precio: 35.000,
+        categoria: "Mujer",
+        tipo: "Ropa",
+        valor: "medio",
+    },
+    {
+        imagen: "/imagenes/ropa/blusasM/3.jpg",
+        nombre: "Join Life",
+        precio: 35.000,
+        categoria: "Mujer",
+        tipo: "Ropa",
+        valor: "medio",
+    },
+    {
+        imagen: "/imagenes/ropa/blusasM/4.jpg",
+        nombre: "Join Life",
+        precio: 45.500,
+        categoria: "Mujer",
+        tipo: "Ropa",
+        valor: "medio",
+    },
+    {
+        imagen: "/imagenes/ropa/blusasM/5.jpg",
+        nombre: "Join Life",
+        precio: 50.200,
+        categoria: "Mujer",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/camisasH/1.jpg",
+        nombre: "Join Life",
+        precio: 50.200,
+        categoria: "Hombre",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/camisasH/2.jpg",
+        nombre: "Join Life",
+        precio: 20.200,
+        categoria: "Hombre",
+        tipo: "Ropa",
+        valor: "bajo",
+    },
+    {
+        imagen: "/imagenes/ropa/camisasH/3.jpg",
+        nombre: "Join Life",
+        precio: 70.200,
+        categoria: "Hombre",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/camisasH/4.jpg",
+        nombre: "Join Life",
+        precio: 30.200,
+        categoria: "Hombre",
+        tipo: "Ropa",
+        valor: "bajo",
+    },
+    {
+        imagen: "/imagenes/ropa/camisasH/5.jpg",
+        nombre: "Join Life",
+        precio: 52.200,
+        categoria: "Hombre",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/novedades/1.jpg",
+        nombre: "Join Life",
+        precio: 50.200,
+        categoria: "Ambos",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/novedades/2.jpg",
+        nombre: "Join Life",
+        precio: 40.200,
+        categoria: "Ambos", 
+        tipo: "Ropa",
+        valor: "medio",
+    
+    },
+    {
+        imagen: "/imagenes/ropa/novedades/3.jpg",
+        nombre: "Join Life",
+        precio: 55.500,
+        categoria: "Ambos",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/novedades/4.jpg",
+        nombre: "Join Life",
+        precio: 146.200,
+        categoria: "Ambos",
+        tipo: "Ropa",
+        valor: "alto",
+    },
+    {
+        imagen: "/imagenes/ropa/novedades/5.jpg",
+        nombre: "Join Life",
+        precio: 150.200,
+        categoria: "Ambos",
+        tipo: "Ropa",
+        valor: "alto",
+    }
+    
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
